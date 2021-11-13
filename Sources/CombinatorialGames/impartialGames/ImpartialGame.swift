9d8a6e2caba5ce32d567b371623352462ad9ef66
Michael Schwarz <michi.schwarz@gmail.com>
2021-11-13 14:49:06 +0100

    Method to calculate whether the first player can win

--- Sources/CombinatorialGames/impartialGames/ImpartialGame.swift
+++ Sources/CombinatorialGames/impartialGames/ImpartialGame.swift
@@ -1,3 +1,28 @@
 protocol ImpartialGame: Hashable {
     var plays: [Self] { get }
 }
+
+/// Encapsulate state used by `firstPlayerWins()` for caching results.
+fileprivate enum FirstPlayerWins {
+    static var cache: [AnyHashable:Bool] = [:]
+    static var walking: Set<AnyHashable> = []
+
+    static func firstPlayerWins<G: ImpartialGame>(game: G) -> Bool {
+        if let result = cache[game] {
+            return result
+        }
+
+        let inserted = walking.insert(game).inserted
+        precondition(inserted, "Recursive call leading up to \(game)")
+
+        let result = !game.plays.allSatisfy(firstPlayerWins)
+        cache[game] = result
+
+        defer { walking.remove(game) }
+        return result
+    }
+}
+
+func firstPlayerWins<G: ImpartialGame>(game: G) -> Bool {
+    FirstPlayerWins.firstPlayerWins(game: game)
+}
--- Sources/CombinatorialGames/main.swift
+++ Sources/CombinatorialGames/main.swift
@@ -1,1 +1,7 @@
-print((BabyRook(5) + King(1, 2)).plays)
+let game = (BabyRook(5) + King(1, 2))
+
+print("\(game): \(firstPlayerWins(game: game))")
+
+for i in game.plays {
+    print("\(i): \(firstPlayerWins(game: i))")
+}
